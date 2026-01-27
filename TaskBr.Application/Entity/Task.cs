0d066404fc4d1675b5d58af1877f190683e1f385
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-01-27 14:22:45 -0300

    Entity e enums

---
+++ TaskBr.Application/Entity/Task.cs
@@ -0,0 +1,12 @@
+﻿using TaskBr.Application.Enums;
+
+namespace TaskBr.Application.Entity;
+
+public class Task {
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public PriorityTask Priority { get; set; }
+    public DateTime DueDate { get; set; }
+    public StatusTask Status { get; set; }
+}
---
+++ TaskBr.Application/Enums/StatusTask.cs
@@ -0,0 +1,7 @@
+﻿namespace TaskBr.Application.Enums;
+
+public enum StatusTask {
+    Completed = 0,
+    Pending = 1,
+    InProgress = 2
+}
