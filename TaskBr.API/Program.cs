8d21e630f7f99c8c51cceea1f53fc21d64e9816b
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-01-27 13:29:21 -0300

    Configs iniciais

--- TaskBr.API/Program.cs
+++ TaskBr.API/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
 
 var app = builder.Build();
 
@@ -12,6 +15,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
 }
 
 app.UseHttpsRedirection();
